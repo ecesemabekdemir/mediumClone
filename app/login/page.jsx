6f18bffb49084abf44f5ed919a019132e7a06243
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-14 18:05:57 +0300

    login sayfasında 7.satır h1 text ekledim. 13.satır h3 text ekledim div kapsayıcıları css e göre ayarladım

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,13 +1,20 @@
 import { login, signup } from "./action";
+import "./login.css";
 
 export default function LoginPage() {
   return (
     <div className="login-form">
+      <h1>Join Medium.</h1>
       <form action="">
         <input type="email" name="email" placeholder="mail adresi gir" />
         <input type="password" name="password" placeholder="********" />
         <button formAction={login}>Login</button>
-        <button formAction={signup}>Signup</button>
+        <div className="signup">
+          <h3>Already have an account?</h3>
+          <button className="btn" formAction={signup}>
+            Signup
+          </button>
+        </div>
       </form>
     </div>
   );
