ecd6c08baf8f9bd3c9d1d61f3ed4a4448a3156bd
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-15 13:24:16 +0300

    style : header ve newpost style eklendi

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <></>;
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Home() {
+  const supabase = createClient();
+
+  let { data: posts } = await supabase.from("posts").select("*");
+
+  console.log(posts);
+
+  return <div className="postsContainer"></div>;
 }
