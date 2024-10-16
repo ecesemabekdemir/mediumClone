0d16bb5beeb623e661019874835020ec23e01ef5
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-16 16:38:47 +0300

    footer eklendi

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer/page";
 import MainHeader from "@/components/header";
 
 export default function MainLayout({ children }) {
@@ -6,6 +7,7 @@ export default function MainLayout({ children }) {
       <div>
         <MainHeader />
         {children}
+        <Footer />
       </div>
     </>
   );
---
+++ components/footer/page.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <div className="footerContainer">
+      <div className="footer">
+        <ul>
+          <li>Help</li>
+          <li>status</li>
+          <li>About</li>
+          <li>Careers</li>
+          <li>Press</li>
+          <li>Blog</li>
+          <li>Privacy</li>
+          <li>Terms</li>
+          <li>Text to sppech</li>
+          <li>Teams</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
